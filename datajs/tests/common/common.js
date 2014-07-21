e7c3a6733a68d4ea556d8351eaaaf3edf5a21e35
Bing Li <bingl@apache.org>
2014-07-21 09:43:33 +0800

    Fix the failure test cases by referring the correct datajs object

---
+++ datajs/tests/common/common.js
@@ -0,0 +1,8 @@
+﻿(function (window, undefined) {
+        window.OData = window.datajs.V4.oData;
+        window.temp = window.datajs.V4;
+        window.temp.store = window.datajs.V4.store;
+        window.temp.cache = window.datajs.V4.cache;
+        window.datajs = window.temp;
+        delete window.temp;
+})(this);
