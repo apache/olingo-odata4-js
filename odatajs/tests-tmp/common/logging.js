29a0817e957938abcef3c24b68a57b29f14fe701
Sven Kobler <sven.kobler-morris@sap.com>
2014-09-02 10:41:22 +0200

    [OLINGO-418] remove tests-tmp

--- odatajs/tests-tmp/common/logging.js
+++
@@ -1,5 +0,0 @@
-QUnit.log( function (context) {
-    if (window.console && window.console.log && context.message) {
-        window.console.log(context.result + ' :: ' + context.message  );
-    }
-});
