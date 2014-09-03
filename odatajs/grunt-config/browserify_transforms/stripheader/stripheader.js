289d0843ac168a333cf063183913a70c9a90e100
Sven Kobler <sven.kobler-morris@sap.com>
2014-09-03 15:02:37 +0200

    [OLINGO-416] fix in browserify tranform stipheader

--- odatajs/grunt-config/browserify_transforms/stripheader/stripheader.js
+++ odatajs/grunt-config/browserify_transforms/stripheader/stripheader.js
@@ -2,14 +2,17 @@ var through = require('through');
 
 module.exports = function (file) {
   //if (/\.json$/.test(file)) return through();
-
+  //console.log('strip header from ' + file);
   var data = "";
+
 
   return through(
     function (buf) { data += buf;    },
     function () {
       try {
-        var out = data.replace(/^(\/\*(.|\n|\r)*?\*\/)/gi,"");
+        //console.log('\nin--------------\na'+data.substring(0,1000));
+        var out = data.replace(/(\/\*(.|\n|\r)*?\*\/)/i,"");
+        //console.log('\nout--------------\n'+out.substring(0,300));
         this.queue(out);
       } catch (er) {
         this.emit("error", new Error(er.toString().replace("Error: ", "") + " (" + file + ")"));
